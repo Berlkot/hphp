5c267beb99d8a0f1d56f57c18d9227ca2d5f6f8f
Berlkot <romicheg0600@mail.ru>
2024-02-13 13:31:14 +0300

    FormLab

---
+++ Lector4/back.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <textarea name="" id="" cols="30" rows="10">
+    <?php
+    print_r(get_headers("https://www.reddit.com/r/godot/comments/17apayy/how_can_i_iterate_through_an_entire_folder_and/"));
+    ?>
+    </textarea>
+    <a href="../Lector4">go back</a>
+
+</body>
+
+</html>
